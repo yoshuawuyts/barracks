0646c7b56c4685c13849ce0cd6692ea2b4a80da8
yoshuawuyts <i@yoshuawuyts.com>
2014-12-28 19:14:34 +0100

    styles: es6ify

--- index.js
+++ index.js
@@ -1,206 +1,161 @@
-/**
- * Module dependences
- */
+/*eslint no-underscore-dangle:0 */
 
-var debug = require('debug')('barracks');
-var assert = require('assertf');
-var async = require('async');
+const debug  = require('debug')('barracks')
+const assert = require('assertf')
+const async  = require('async')
 
-/**
- * Exports
- */
+module.exports = Dispatcher
 
-module.exports = Dispatcher;
+const dispatcher = Dispatcher.prototype
 
-/**
- * Dispatcher prototype
- */
-
-var dispatcher = Dispatcher.prototype;
-
-/**
- * Initialize the dispatcher with an
- * 'actions' object.
- *
- * @return {Object}
- * @api public
- */
-
+// Initialize the dispatcher with an
+// 'actions' object.
+// @return {Object}
 function Dispatcher(actions) {
 
-  if (!(this instanceof Dispatcher)) return new Dispatcher(actions);
+  if (!(this instanceof Dispatcher)) return new Dispatcher(actions)
 
-  assert(actions, 'an \'actions\' object should be passed as an argument');
-  assert.equal(typeof actions, 'object', 'actions should be an object');
-  _assertActionsObject(actions);
+  assert(actions, 'an \'actions\' object should be passed as an argument')
+  assert.equal(typeof actions, 'object', 'actions should be an object')
+  _assertActionsObject(actions)
 
-  this.locals = {};
-  this.payload = null;
+  this.locals  = {}
+  this.payload = null
 
-  this._current = [];
-  this._isPending = {};
-  this._isHandled = {};
-  this._actions = actions;
-  this._isDispatching = false;
+  this._current       = []
+  this._isPending     = {}
+  this._isHandled     = {}
+  this._actions       = actions
+  this._isDispatching = false
 
-  return this.dispatch.bind(this);
-};
-
-/**
- * Dispatch event to stores.
- *
- * @param {String} action
- * @param {Object | Object[]} data
- * @return {Function[]}
- * @api public
- */
-
-dispatcher.dispatch = function(action, payload) {
-
-  assert.equal(typeof action, 'string', 'action \'%s\' should be a string', action);
-  assert(!this._isDispatching, 'cannot dispatch \'%s\' in the middle of a dispatch', action);
-
-  this._current.push(action);
-  this._isDispatching = true;
-
-  this._isPending = {};
-  this._isPending[action] = true;
-
-  this._isHandled = {};
-  this._isHandled[action] = false;
-
-  this.locals = {};
-  this.payload = payload;
-
-  try {
-    var fn = _getAction.call(this, action);
-  } catch (e) {
-    _stopDispatching.call(this);
-    throw e;
-  }
-
-  debug('dispatch \'%s\'', action);
-  fn.call(this, _stopDispatching.bind(this));
-};
-
-/**
- * Expose a delegation method to the registered actions. Calls `async.series()`
- * under the hood. Sets a 'pending' state to
- *
- * @param {String[] |} ids
- * @param {Function} done
- * @api public
- */
-
-dispatcher.waitFor = function(actions, done) {
-  done = done || function() {};
-  assert.equal(typeof done, 'function', 'callback should be a function');
-
-  actions = Array.isArray(actions) ? actions : [actions];
-
-  var arr = actions.map(function(action) {
-
-    var fn = _getAction.call(this, action);
-    var nwFn = _thunkify.call(this, fn, action);
-    return nwFn.bind(this);
-
-  }.bind(this));
-
-  var nwArr = arr.concat(done.bind(this));
-  async.series(nwArr);
-};
-
-/**
- * Deep assert if the passed actions object is correct.
- *
- * @param {Object} actions
- * @api private
- */
-
-function _assertActionsObject(actions) {
-  Object.keys(actions).forEach(function(key) {
-    var action = actions[key];
-    if ('object' == typeof action) return _assertActionsObject(action);
-    assert.equal(typeof action, 'function', 'action should be a function');
-  });
+  return this.dispatch.bind(this)
 }
 
-/**
- * Thunkify a function to properly set
- * `this._isHandled[action] = true` when
- * done executing.
- *
- * @param {Function} fn
- * @param {Function} isHandled
- * @return {Function}
- */
+// Dispatch event to stores.
+// @param {String} action
+// @param {Object | Object[]} data
+// @return {Function[]}
+dispatcher.dispatch = function(action, payload) {
 
+  assert.equal(typeof action, 'string', 'action \'%s\' should be a string', action)
+  assert(!this._isDispatching, 'cannot dispatch \'%s\' in the middle of a dispatch', action)
+
+  this._current.push(action)
+  this._isDispatching = true
+
+  this._isPending         = {}
+  this._isPending[action] = true
+
+  this._isHandled         = {}
+  this._isHandled[action] = false
+
+  this.locals  = {}
+  this.payload = payload
+
+  try {
+    const fn = _getAction.call(this, action)
+  } catch (e) {
+    _stopDispatching.call(this)
+    throw e
+  }
+
+  debug('dispatch \'%s\'', action)
+  fn.call(this, _stopDispatching.bind(this))
+}
+
+// Expose a delegation method to the registered
+// actions. Calls `async.series()` under the hood.
+// @param {String | String[]} actions
+// @param {Function} done
+dispatcher.waitFor = function(actions, done) {
+  done = done || function() {}
+  assert.equal(typeof done, 'function', 'callback should be a function')
+
+  actions = Array.isArray(actions) ? actions : [actions]
+  const ctx = this
+
+  const arr = actions.map(function(action) {
+    const fn = _getAction.call(ctx, action)
+    const nwFn = _thunkify.call(ctx, fn, action)
+    return nwFn.bind(ctx)
+  })
+
+  const nwArr = arr.concat(done.bind(this))
+  async.series(nwArr)
+}
+
+// Deep assert if the passed actions object is correct.
+// @param {Object} actions
+function _assertActionsObject(actions) {
+  Object.keys(actions).forEach(function(key) {
+    const action = actions[key]
+    if ('object' == typeof action) return _assertActionsObject(action)
+    assert.equal(typeof action, 'function', 'action should be a function')
+  })
+}
+
+// Thunkify a function to properly set
+// `this._isHandled[action] = true` when
+// done executing.
+// @param {Function} fn
+// @param {Function} isHandled
+// @return {Function}
 function _thunkify(fn, action) {
 
   return function(done) {
     try {
-      assert.equal(typeof action, 'string', '.waitFor(): requires a string or array of strings');
+      assert.equal(typeof action, 'string', '.waitFor(): requires a string or array of strings')
       if (this._isPending[action]) {
-        assert(this._isHandled[action], 'circular dependency detected while waiting for \'%s\'', action);
+        assert(this._isHandled[action], 'circular dependency detected while waiting for \'%s\'', action)
       }
     } catch(e) {
-      _stopDispatching.call(this);
-      throw e;
+      _stopDispatching.call(this)
+      throw e
     }
 
-    this._isPending[action] = true;
-    this._isHandled[action] = false;
+    this._isPending[action] = true
+    this._isHandled[action] = false
 
     function fin() {
-      this._current.pop();
-      this._isHandled[action] = true;
-      done();
+      this._current.pop()
+      this._isHandled[action] = true
+      done()
     }
 
-    this._current.push(action);
-    debug('\'%s\' -> \'%s\'', this._current[this._current.length - 2], action);
-    fn(fin.bind(this));
+    this._current.push(action)
+    debug('\'%s\' -> \'%s\'', this._current[this._current.length - 2], action)
+    fn(fin.bind(this))
   }
 }
 
-/**
- * Get the dispatched action. Traverses the call stack
- * recursively until a function is found.
- *
- * @param {String[]} arr
- * @param {String} action
- * @return {Function}
- * @api private
- */
-
+// Get the dispatched action. Traverses the call stack
+// recursively until a function is found.
+// @param {String[]} arr
+// @param {String} action
+// @return {Function}
 function _getAction(action, arr, index) {
-  arr = arr || action.split('_');
-  index = index || 0;
-  var val = arr[index];
+  arr = arr || action.split('_')
+  index = index || 0
+  const val = arr[index]
 
-  if ('object' == typeof val) return _getAction.call(this, action, arr, index++);
+  if ('object' == typeof val) return _getAction.call(this, action, arr, index++)
 
-  var fn = this._actions;
+  var fn = this._actions
   arr.forEach(function(obj, i) {
-    assert(fn[arr[i]], 'action \'%s\' is not registered', action);
-    fn = fn[arr[i]];
-  }.bind(this));
+    assert(fn[arr[i]], 'action \'%s\' is not registered', action)
+    fn = fn[arr[i]]
+  })
 
-  assert.equal(typeof fn, 'function', 'action \'%s\' is not registered', action);
-  return fn.bind(this);
+  assert.equal(typeof fn, 'function', 'action \'%s\' is not registered', action)
+  return fn.bind(this)
 }
 
-/**
- * Clear bookkeeping.
- *
- * @api private
- */
-
+// Clear bookkeeping.
 function _stopDispatching() {
-  this._isDispatching = false;
-  this._isPending = {};
-  this._isHandled = {};
-  this.payload = null;
-  this._current = [];
-  this.locals = {};
+  this._isDispatching = false
+  this._isPending = {}
+  this._isHandled = {}
+  this.payload = null
+  this._current = []
+  this.locals = {}
 }

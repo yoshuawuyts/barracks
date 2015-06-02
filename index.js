d77d659fd02962ed3db00920f86fb96dfcf5b333
Yoshua Wuyts <i@yoshuawuyts.com>
2015-06-03 01:00:16 +0200

    styles: clean up

--- index.js
+++ index.js
@@ -1,55 +1,46 @@
-/*eslint no-underscore-dangle:0 */
-
-const debug  = require('debug')('barracks')
+const debug = require('debug')('barracks')
 const assert = require('assertf')
-const async  = require('async')
+const async = require('async')
 
 module.exports = Dispatcher
 
-const dispatcher = Dispatcher.prototype
-
-// Initialize the dispatcher with an
-// 'actions' object.
-// @return {Object}
-function Dispatcher(actions) {
-
+// initialize the dispatcher with actions
+// obj -> fn
+function Dispatcher (actions) {
   if (!(this instanceof Dispatcher)) return new Dispatcher(actions)
 
-  assert(actions, 'an \'actions\' object should be passed as an argument')
+  assert(actions, "an 'actions' object should be passed as an argument")
   assert.equal(typeof actions, 'object', 'actions should be an object')
   _assertActionsObject(actions)
 
-  this.locals  = {}
+  this.locals = {}
   this.payload = null
 
-  this._current       = []
-  this._isPending     = {}
-  this._isHandled     = {}
-  this._actions       = actions
+  this._current = []
+  this._isPending = {}
+  this._isHandled = {}
+  this._actions = actions
   this._isDispatching = false
 
   return this.dispatch.bind(this)
 }
 
-// Dispatch event to stores.
-// @param {String} action
-// @param {Object | Object[]} data
-// @return {Function[]}
-dispatcher.dispatch = function(action, payload) {
-
-  assert.equal(typeof action, 'string', 'action \'%s\' should be a string', action)
-  assert(!this._isDispatching, 'cannot dispatch \'%s\' in the middle of a dispatch', action)
+// dispatch event to stores
+// str, obj|[obj] -> fn
+Dispatcher.prototype.dispatch = function (action, payload) {
+  assert.equal(typeof action, 'string', "action '%s' should be a string", action)
+  assert(!this._isDispatching, "cannot dispatch '%s' in the middle of a dispatch", action)
 
   this._current.push(action)
   this._isDispatching = true
 
-  this._isPending         = {}
+  this._isPending = {}
   this._isPending[action] = true
 
-  this._isHandled         = {}
+  this._isHandled = {}
   this._isHandled[action] = false
 
-  this.locals  = {}
+  this.locals = {}
   this.payload = payload
 
   try {
@@ -59,22 +50,21 @@ dispatcher.dispatch = function(action, payload) {
     throw e
   }
 
-  debug('dispatch \'%s\'', action)
+  debug("dispatch '%s'", action)
   fn.call(this, _stopDispatching.bind(this))
 }
 
-// Expose a delegation method to the registered
-// actions. Calls `async.series()` under the hood.
-// @param {String | String[]} actions
-// @param {Function} done
-dispatcher.waitFor = function(actions, done) {
-  done = done || function() {}
+// expose a delegation method to the registered
+// actions. Calls `async.series()` under the hood
+// str|[str], fn -> null
+Dispatcher.prototype.waitFor = function (actions, done) {
+  done = done || function () {}
   assert.equal(typeof done, 'function', 'callback should be a function')
 
   actions = Array.isArray(actions) ? actions : [actions]
   const ctx = this
 
-  const arr = actions.map(function(action) {
+  const arr = actions.map(function (action) {
     const fn = _getAction.call(ctx, action)
     const nwFn = _thunkify.call(ctx, fn, action)
     return nwFn.bind(ctx)
@@ -84,29 +74,25 @@ dispatcher.waitFor = function(actions, done) {
   async.series(nwArr)
 }
 
-// Deep assert if the passed actions object is correct.
-// @param {Object} actions
-function _assertActionsObject(actions) {
-  Object.keys(actions).forEach(function(key) {
+// deep assert the actions object
+// obj -> null
+function _assertActionsObject (actions) {
+  Object.keys(actions).forEach(function (key) {
     const action = actions[key]
     if ('object' == typeof action) return _assertActionsObject(action)
     assert.equal(typeof action, 'function', 'action should be a function')
   })
 }
 
-// Thunkify a function to properly set
-// `this._isHandled[action] = true` when
-// done executing.
-// @param {Function} fn
-// @param {Function} isHandled
-// @return {Function}
-function _thunkify(fn, action) {
-
-  return function(done) {
+// wrap function to set
+// `this._isHandled[action]` on end
+// fn, fn -> fn
+function _thunkify (fn, action) {
+  return function (done) {
     try {
       assert.equal(typeof action, 'string', '.waitFor(): requires a string or array of strings')
       if (this._isPending[action]) {
-        assert(this._isHandled[action], 'circular dependency detected while waiting for \'%s\'', action)
+        assert(this._isHandled[action], "circular dependency detected while waiting for '%s'", action)
       }
     } catch(e) {
       _stopDispatching.call(this)
@@ -116,24 +102,21 @@ function _thunkify(fn, action) {
     this._isPending[action] = true
     this._isHandled[action] = false
 
-    function fin() {
+    function fin () {
       this._current.pop()
       this._isHandled[action] = true
       done()
     }
 
     this._current.push(action)
-    debug('\'%s\' -> \'%s\'', this._current[this._current.length - 2], action)
+    debug("'%s' -> '%s'", this._current[this._current.length - 2], action)
     fn(fin.bind(this))
   }
 }
 
-// Get the dispatched action. Traverses the call stack
-// recursively until a function is found.
-// @param {String[]} arr
-// @param {String} action
-// @return {Function}
-function _getAction(action, arr, index) {
+// get the dispatched action recursively
+// [str], str -> fn
+function _getAction (action, arr, index) {
   arr = arr || action.split('_')
   index = index || 0
   const val = arr[index]
@@ -141,17 +124,18 @@ function _getAction(action, arr, index) {
   if ('object' == typeof val) return _getAction.call(this, action, arr, index++)
 
   var fn = this._actions
-  arr.forEach(function(obj, i) {
-    assert(fn[arr[i]], 'action \'%s\' is not registered', action)
+  arr.forEach(function (obj, i) {
+    assert(fn[arr[i]], "action '%s' is not registered", action)
     fn = fn[arr[i]]
   })
 
-  assert.equal(typeof fn, 'function', 'action \'%s\' is not registered', action)
+  assert.equal(typeof fn, 'function', "action '%s' is not registered", action)
   return fn.bind(this)
 }
 
-// Clear bookkeeping.
-function _stopDispatching() {
+// reset internal state
+// null -> null
+function _stopDispatching () {
   this._isDispatching = false
   this._isPending = {}
   this._isHandled = {}
--- test.js
+++ test.js
@@ -1,8 +1,7 @@
-
-const test     = require('tape')
+const test = require('tape')
 const barracks = require('./index.js')
 
-test('barracks() should assert argument types', function(t) {
+test('barracks() should assert argument types', function (t) {
   t.plan(4)
   t.throws(barracks.bind(barracks), /object should be passed as/)
   t.throws(barracks.bind(barracks, 123), /actions should be an object/)
@@ -10,28 +9,28 @@ test('barracks() should assert argument types', function(t) {
   t.throws(barracks.bind(barracks, {users: {add: 123}}), 'action should be an function')
 })
 
-test('dispatcher = barracks() should return a function', function(t) {
+test('dispatcher = barracks() should return a function', function (t) {
   t.plan(1)
 
   const dispatcher = barracks({
     users: {
-      add: function() {},
-      remove: function() {}
+      add: function () {},
+      remove: function () {}
     },
     courses: {
-      get: function() {},
-      put: function() {}
+      get: function () {},
+      put: function () {}
     }
   })
 
   t.equal(typeof dispatcher, 'function')
 })
 
-test('dispatcher() should assert argument types', function(t) {
+test('dispatcher() should assert argument types', function (t) {
   t.plan(4)
 
   const dispatcher = barracks({
-    foo: {bar: {baz: function(){}}}
+    foo: {bar: {baz: function () {}}}
   })
 
   t.throws(dispatcher.bind(dispatcher, {}), /should be a string/)
@@ -40,82 +39,82 @@ test('dispatcher() should assert argument types', function(t) {
   t.throws(dispatcher.bind(dispatcher, 'foo_bar_baz_err'), /is not registered/)
 })
 
-test('dispatcher() should call a cb when done', function(t) {
+test('dispatcher() should call a cb when done', function (t) {
   t.plan(1)
 
   const dispatcher = barracks({
     users: {
-      add: function() {},
-      remove: function() {}
+      add: function () {},
+      remove: function () {}
     },
     courses: {
-      get: function() {},
-      put: function() {this.payload()}
+      get: function () {},
+      put: function () {this.payload()}
     }
   })
 
-  dispatcher('courses_put', function() {
+  dispatcher('courses_put', function () {
     t.ok(true, 'cb called')
   })
 })
 
-test('dispatcher should throw if called while in progress', function(t) {
+test('dispatcher should throw if called while in progress', function (t) {
   t.plan(2)
   const dispatcher = barracks({
     users: {
-      add: function() {t.fails()},
-      remove: function() {t.fails()}
+      add: function () {t.fails()},
+      remove: function () {t.fails()}
     },
     courses: {
-      get: function() {t.fails()},
-      put: function() {
+      get: function () {t.fails()},
+      put: function () {
         t.throws(dispatcher.bind(this, 'users_add'), /in the middle of a dispatch/)
         this.payload()
       }
     }
   })
 
-  dispatcher('courses_put', function() {t.ok(true, 'end')})
+  dispatcher('courses_put', function () {t.ok(true, 'end')})
 })
 
-test('dispatcher.waitFor() should assert input', function(t) {
+test('dispatcher.waitFor() should assert input', function (t) {
   t.plan(2)
   const dispatcher = barracks({
     courses: {
-      get: function() {t.fail()},
-      put: function() {
+      get: function () {t.fail()},
+      put: function () {
         t.throws(dispatcher.bind(this, 'courses_get'), /in the middle of a dispatch/)
         this.payload()
       }
     }
   })
 
-  dispatcher('courses_put', function() {
+  dispatcher('courses_put', function () {
     t.ok(true, 'end')
   })
 })
 
-test('dispatcher.waitFor should wat for subcalls to next', function(t) {
+test('dispatcher.waitFor should wat for subcalls to next', function (t) {
   t.plan(2)
 
   var count = 0
 
   const dispatcher = barracks({
     users: {
-      init: function(next) {
-        this.waitFor(['users_foo', 'users_bar'], function() {
+      init: function (next) {
+        this.waitFor(['users_foo', 'users_bar'], function () {
           t.equal(count, 2)
           this.payload()
         })
       },
-      foo: function(next) {
-        setTimeout(function() {
+      foo: function (next) {
+        setTimeout(function () {
           count++
           next()
         }, 10)
       },
-      bar: function(next) {
-        setTimeout(function() {
+      bar: function (next) {
+        setTimeout(function () {
           count++
           next()
         }, 5)
@@ -123,25 +122,25 @@ test('dispatcher.waitFor should wat for subcalls to next', function(t) {
     }
   })
 
-  dispatcher('users_init', function() {
+  dispatcher('users_init', function () {
     t.ok(true, 'end')
   })
 })
 
-test('dispatcher.waitFor() should catch circular dependencies', function(t) {
+test('dispatcher.waitFor() should catch circular dependencies', function (t) {
   t.plan(2)
   const dispatcher = barracks({
     courses: {
-      foo: function(next) {
-        this.waitFor('courses_get', function() {
+      foo: function (next) {
+        this.waitFor('courses_get', function () {
           this.locals.fn(true, 'done')
         })
       },
-      get: function(next) {
+      get: function (next) {
         this.locals.fn = t.ok
         next()
       },
-      put: function(next) {
+      put: function (next) {
         t.throws(this.waitFor.bind(this, 'courses_put'), /circular dependency/)
         next()
       }
@@ -152,21 +151,21 @@ test('dispatcher.waitFor() should catch circular dependencies', function(t) {
   dispatcher('courses_foo')
 })
 
-test('dispatcher.waitFor() should not throw if a fn has already been called', function(t) {
+test('dispatcher.waitFor() should not throw if a fn has already been called', function (t) {
   t.plan(1)
 
   const dispatcher = barracks({
     courses: {
-      get: function(next) {
+      get: function (next) {
         next()
       },
-      foo: function(next) {
-        this.waitFor('courses_get', function() {
+      foo: function (next) {
+        this.waitFor('courses_get', function () {
           next()
         })
       },
-      bar: function(next) {
-        this.waitFor(['courses_get', 'courses_get', 'courses_foo'], function() {
+      bar: function (next) {
+        this.waitFor(['courses_get', 'courses_get', 'courses_foo'], function () {
           t.ok(true, 'end')
           next()
         })
@@ -177,18 +176,18 @@ test('dispatcher.waitFor() should not throw if a fn has already been called', fu
   dispatcher('courses_bar')
 })
 
-test('ctx.locals should exist in a shared context', function(t) {
+test('ctx.locals should exist in a shared context', function (t) {
   t.plan(2)
 
   const dispatcher = barracks({
     courses: {
-      get: function(next) {
+      get: function (next) {
         this.locals.done = this.payload
         next()
       },
-      put: function(next) {
+      put: function (next) {
         t.equal(typeof this.payload, 'function')
-        this.waitFor('courses_get', function() {
+        this.waitFor('courses_get', function () {
           this.locals.done(true, 'end')
         })
       }
